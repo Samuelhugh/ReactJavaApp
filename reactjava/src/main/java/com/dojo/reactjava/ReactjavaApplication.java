d44d338066cdb5621a72c8941ec945900856738a
samuel hughes <samuelhugh33@gmail.com>
2022-09-06 11:25:31 -0500

    Created React frontend and Java Rest API backend

---
+++ reactjava/src/main/java/com/dojo/reactjava/ReactjavaApplication.java
@@ -0,0 +1,13 @@
+package com.dojo.reactjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactjavaApplication.class, args);
+	}
+
+}
---
+++ reactjava/src/test/java/com/dojo/reactjava/ReactjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dojo.reactjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
